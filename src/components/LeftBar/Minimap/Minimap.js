a5e37e3ea38c7f0744f4b5a50b33f1e4f36699af
filipe <filipegroh@gmail.com>
2019-05-20 17:03:13 -0300

    init 2

---
+++ src/components/LeftBar/Minimap/Minimap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Minimap.module.scss';
+
+const Minimap = props => {
+  return (
+    <div>
+      <div>
+        <img />
+      </div>
+    </div>
+  );
+};
+
+export default Minimap;
--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,12 +1,26 @@
-import React from "react";
-import styles from "./Main.module.scss";
-import LeftBar from "components/LeftBar/LeftBar";
-import RightBar from "components/RightBar/RightBar";
-import TextScreen from "components/TextScreen/TextScreen";
-import { Grid } from "@material-ui/core";
-import ActionBar from "components/ActionBar/ActionBar";
+import React from 'react';
+import styles from './Main.module.scss';
+import LeftBar from 'components/LeftBar/LeftBar';
+import RightBar from 'components/RightBar/RightBar';
+import TextScreen from 'components/TextScreen/TextScreen';
+import { Grid } from '@material-ui/core';
+import ActionBar from 'components/ActionBar/ActionBar';
+import FreePort from 'data/FreePort.json';
+import { fromJS } from 'immutable';
 
 export default class Main extends React.Component {
+
+  state = {
+    location: fromJS(FreePort),
+    currentLocation: [],
+    player: null,
+  }
+  componentDidMount = () => {
+    console.log(this.state.location)
+    this.setState({
+      currentLocation: location.filter
+    })
+  };
   render = () => {
     return (
       <Grid container className={styles.mainGrid}>
